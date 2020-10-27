16361584caae3cde6179a956cb99ea280c778f2c
Brian Pulfer <42559439+BrianPulfer@users.noreply.github.com>
2020-10-27 22:02:31 +0100

    Added a dropdown menu to change the simulated camera resolution

---
+++ sdsim/Assets/Scripts/ResolutionSetter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionSetter : MonoBehaviour
+{
+    // DropDown from the Menu canvas
+    Dropdown dropdown;
+
+    // Current selection message from the dropdown
+    string message;
+
+    // Text of the current selection
+    public Text text;
+
+    // Index of the current selection
+    int dropDownValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown = GetComponent<Dropdown>();
+
+        dropdown.onValueChanged.AddListener(
+            delegate
+            {
+                DropdownValueChanged(dropdown);
+            });
+    }
+
+    void DropdownValueChanged(Dropdown change)
+    {
+        setCameraSensorRes(dropdown.options[change.value].text);
+    }
+
+    void setCameraSensorRes(string resolution)
+    {
+        CameraSensor.width = System.Convert.ToInt32(resolution.Split('x')[0]);
+        CameraSensor.height = System.Convert.ToInt32(resolution.Split('x')[1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
