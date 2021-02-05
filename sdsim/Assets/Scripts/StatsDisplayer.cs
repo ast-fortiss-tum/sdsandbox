efa84688763f92564efcffd7465385f8c6ddfaab
Brian Pulfer <42559439+BrianPulfer@users.noreply.github.com>
2021-02-05 10:43:29 +0100

    Dealing with non-yet spawned car (bug in builded project). Added 'DonkeyCar' tag to the car.

--- sdsim/Assets/Scripts/StatsDisplayer.cs
+++ sdsim/Assets/Scripts/StatsDisplayer.cs
@@ -11,16 +11,19 @@ public class StatsDisplayer : MonoBehaviour
     // Stats
     public static int lap;
     public static float time;
+    public static float prevTime;
     public static int offTrackCounter;
     public static float xte;
 
     // Parameters
     public float offTrackXTE; // 1.4
     private bool isOffTrack = false;
+    private float startTime;
 
     // Labels to update
     private Text lapLabel;
     private Text timeLabel;
+    private Text prevTimeLabel;
     private Text outOfTrackLabel;
     private Text xteLabel;
 
@@ -35,17 +38,25 @@ public class StatsDisplayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
+
         // Initializing stats
         lap = 1;
         time = 0;
+        prevTime = 0;
         offTrackCounter = 0;
         xte = 0;
 
         // Initializing PM
-        pm = GameObject.FindObjectOfType<PathManager>();
+        pm = FindObjectOfType<PathManager>();
 
         // Initializing Car
-        car = (Car)(GameObject.Find("Donkey(Clone)").GetComponent<ICar>());
+        GameObject carObj = GameObject.FindGameObjectWithTag("DonkeyCar");
+
+        if (carObj != null)
+        {
+            car = (Car)carObj.GetComponent<ICar>();
+        }
 
         // Getting labels to update
         GameObject statsPanel = GameObject.Find("StatsPanel");
@@ -53,14 +64,16 @@ public class StatsDisplayer : MonoBehaviour
         {
             Text[] labels = statsPanel.GetComponentsInChildren<Text>();
 
-            if (labels.Length >= 4)
+            if (labels.Length >= 5)
             {
                 lapLabel = labels[0];
                 timeLabel = labels[1];
                 outOfTrackLabel = labels[2];
                 xteLabel = labels[3];
+                prevTimeLabel = labels[4];
             }
         }
+
     }
 
     // Update is called once per frame
@@ -74,34 +87,57 @@ public class StatsDisplayer : MonoBehaviour
     {
         lapLabel.text = lapLabel.text.Split(new string[] { ": "}, StringSplitOptions.None)[0] + ": " +lap;
         timeLabel.text = timeLabel.text.Split(new string[] { ": " }, StringSplitOptions.None)[0] + ": " + time;
+        prevTimeLabel.text = prevTimeLabel.text.Split(new string[] { ": " }, StringSplitOptions.None)[0] + ": " + prevTime;
         outOfTrackLabel.text = outOfTrackLabel.text.Split(new string[] { ": " }, StringSplitOptions.None)[0] + ": " + offTrackCounter;
         xteLabel.text = xteLabel.text.Split(new string[] { ": " }, StringSplitOptions.None)[0] + ": " + xte;
     }
 
     private void getUpdatedStats()
     {
+        if(car == null)
+        {
+            GameObject carObj = GameObject.FindGameObjectWithTag("DonkeyCar");
+
+            if (carObj != null)
+            {
+                car = (Car)carObj.GetComponent<ICar>();
+            }
+
+            if(car == null) { return; }
+        }
+
         // Updating lap
-        lap = car.nrExitedTriggers + 1;
+        if (car != null && lap < car.nrExitedTriggers + 1)
+        {
+            lap = car.nrExitedTriggers + 1;
+
+            float finishTime = Time.time;
+            prevTime = finishTime- startTime;
+            startTime = finishTime;
+        }
 
         // Updating time
-        time = Time.time;
+        time = Time.time - startTime;
 
         // Updating XTE
         // pm.path.GetCrossTrackErr(car.GetTransform().position + (car.GetTransform().forward * car.GetVelocity().magnitude), ref xte); // Alternative
-        pm.path.GetCrossTrackErr(car.GetTransform().position, ref xte);
+        if (pm != null && car != null)
+        {
+            pm.path.GetCrossTrackErr(car.GetTransform().position, ref xte);
 
-        // Updating Out-of-track counter
-        if(Math.Abs(xte) > Math.Abs(offTrackXTE))
-        {
-            if (!isOffTrack)
+            // Updating Out-of-track counter
+            if (Math.Abs(xte) > Math.Abs(offTrackXTE))
             {
-                offTrackCounter += 1;
-                isOffTrack = true;
+                if (!isOffTrack)
+                {
+                    offTrackCounter += 1;
+                    isOffTrack = true;
+                }
             }
-        }
-        else
-        {
-            isOffTrack = false;
+            else
+            {
+                isOffTrack = false;
+            }
         }
     }
 }
